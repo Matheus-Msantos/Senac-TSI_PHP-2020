b37c4dd17f2dce1fbde885891f523cdee47f03bf
Matheus-Msantos <matheusmelosantos10@gmail.com>
2020-11-26 22:22:44 -0300

    aula 12/11

---
+++ login/usuario/index.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require '../sessao.php';
+
+require 'model/dados.php';
+
+chdir( __DIR__ );
+
+if ( isset($_GET['apagar']) ) {
+
+	if ( !apagar_usuario( $_GET['apagar'] ) ) {
+
+		$erro = 'Erro ao tentar apagar o usuário!';
+	}
+}
+
+$lista = listar();
+
+require 'view/lista.php';
---
+++ login/usuario/lista.php
@@ -0,0 +1,32 @@
+<?php
+
+include '../header_tpl.php';
+include '../index_menu_tpl.php';
+
+echo '	<br><br>
+		<center>';
+
+if ( isset($erro) ) {
+
+	echo "<font color='red'>$erro</font><br><br>";
+}
+
+echo '	<table border="1" width="50%">
+			<tr>
+				<td>ID</td><td>Nome</td><td>E-mail</td><td></td>
+			</tr>';
+
+foreach ($lista as $usuario) {
+	
+	echo "	<tr>
+				<td>{$usuario['id']}</td>
+				<td>{$usuario['nome']}</td>
+				<td>{$usuario['email']}</td>
+				<td><a href='?apagar={$usuario['id']}'>apagar</a></td>
+			</tr>";
+}
+
+echo '	</table>
+		</center>';
+
+include '../footer_tpl.php';
